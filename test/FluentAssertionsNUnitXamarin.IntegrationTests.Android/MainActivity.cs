cc8d690e1fe38e2c7edfcc1133fb7c55f14be1fc
Mahmoud Ali <muddibr@gmail.com>
2019-12-26 08:25:47 -0400

    Testes nUnit.Xamarin

---
+++ test/FluentAssertionsNUnitXamarin.IntegrationTests.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using NUnit.Runner.Services;
+
+namespace FluentAssertionsNUnitXamarin.IntegrationTests.Droid
+{
+    [Activity(Label = "FluentAssertionsNUnitXamarin.IntegrationTests", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            // This will load all tests within the current project
+            var nunit = new NUnit.Runner.App();
+
+            // If you want to add tests in another assembly
+            //nunit.AddTestAssembly(typeof(MyTests).Assembly);
+
+            // Do you want to automatically run tests when the app starts?
+            nunit.Options = new TestOptions
+            {
+                AutoRun = true
+            };
+
+            LoadApplication(nunit);
+        }
+    }
+}
---
+++ test/FluentAssertionsNUnitXamarin.IntegrationTests/Specs/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace FluentAssertionsNUnitXamarin.IntegrationTests.Specs
+{
+    public class Test
+    {
+        [Test]
+        public void ListaDeveConter42()
+        {
+            var lista = new List<int> { 1, 42, 100 };
+
+            lista.Should().Contain(42);
+        }
+    }
+}
